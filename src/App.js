4ebafcf36f555fd8ddd8ebf88a8ff31955d65c7f
Masud Rana <masudrana2779@gmail.com>
2021-12-05 18:25:20 +0600

    home page try to done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
-import './s.scss';
 import HomePage from "./pages/homepage/homepage.component";
 
+
+const HatsPage = () => {
+    return(
+        <div>
+            <h1><a href="/hats-details">HatsPageDetails</a></h1>
+            <h1><a href="/">Home</a></h1>
+        </div>
+    )
+}
+
+const HatsPageDetails = () => {
+    return(
+        <div>
+            <h1><a href="/">Home</a></h1>
+            <h1><a href="/hats">HatsPage</a></h1>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+
+    return (
+        <div>
+            <Switch>
+                <Route path={'/'} exact component={HomePage}/>
+                <Route path={'/hats'} exact component={HatsPage}/>
+                <Route path={'/hats-details/:id'} exact component={HatsPageDetails}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
